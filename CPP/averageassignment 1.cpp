5718fc46affb524f7c07e7e34748be2a748209fe
Vishvam Salwan <vsalwan_be20@thapar.edu>
2022-10-12 14:13:55 +0530

    added some cpp codes

---
+++ CPP/averageassignment 1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float a,b,c,avg;
+	cout<<"Enter the three no's";
+	cin>>a>>b>>c;
+	 avg =(a+b+c)/3;
+	cout<<"Average of three no's is \n"<<avg;
+	return 0;
+}
